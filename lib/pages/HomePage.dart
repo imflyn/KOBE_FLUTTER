631f4826a97afbba59273e184159bab0a5fa8f50
yuyingjie <yuyingjie@kedacom.com>
2020-12-28 16:41:04 +0800

    轮播图

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,23 +11,25 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Swiper(
-        //   itemBuilder: (BuildContext context, int index) {
-        //     return new Image.network(
-        //       "http://via.placeholder.com/350x150",
-        //       fit: BoxFit.fill,
-        //     );
-        //   },
-        //   itemCount: 3,
-        //   pagination: new SwiperPagination(),
-        //   control: new SwiperControl(),
-        // ),
-        ListView(
-          children: [Text("aaaaaaaaaaa"), Text("aaaaaaaaaaa"), Text("aaaaaaaaaaa"), Text("aaaaaaaaaaa")],
-        )
-      ],
+    return Container(
+      child: ConstrainedBox(
+          child: Swiper(
+            autoplayDisableOnInteraction: true,
+            autoplayDelay: 8000,
+            duration: 500,
+            loop: true,
+            outer: false,
+            autoplay: true,
+            itemBuilder: (BuildContext context, int index) {
+              return Image.network(
+                "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=360910958,2971105829&fm=26&gp=0.jpg",
+                fit: BoxFit.fill,
+              );
+            },
+            pagination: SwiperPagination(margin: new EdgeInsets.all(5.0), builder: DotSwiperPaginationBuilder(size: 7)),
+            itemCount: 3,
+          ),
+          constraints: BoxConstraints.loose(new Size(MediaQuery.of(context).size.width, 200.0))),
     );
   }
 }
